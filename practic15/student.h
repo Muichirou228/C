a0cfb6eaf7debf2fa6751aae4041e298e88c18af
Mui <153007148+Muichirou228@users.noreply.github.com>
2024-05-10 19:25:13 +0300

    Update student.h

--- practic15/student.h
+++ practic15/student.h
@@ -1,1 +1,32 @@
+//
+// Created by 79627 on 01.04.2024.
+//
+
+#ifndef UNTITLED_STUDENT_H
+#define UNTITLED_STUDENT_H
+#include <stdio.h>
+#include <stdlib.h>
+struct Student
+{
+    char fam[20];
+    char name[20];
+    char sex[2];
+    int yo;
+    char group[10];
+    int MathN;
+    int PhysN;
+    int ChemN;
+    int jourN;
+    void* (*print) (void* student);
+};
+
+void* student_print (void* student);
+
+void* student_input (void* stud);
+
+void* student_input_with_file (void* stud, FILE* file);
+
+void* student_input_with_binary_file (void* stud, FILE* file);
+
+#endif //UNTITLED_STUDENT_H
 
